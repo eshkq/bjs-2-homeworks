9e70bc2fe7461a4ecea664f03771ffd017f51ba8
eshkq <eshkaflex@yandex.ru>
2025-11-16 12:27:03 +0400

    Задача 1 && Задача 2 (arrays)

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,35 @@
+"use strict";
+
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every(function (value, index) {
+    return value === arr2[index];
+  });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if (users.length === 0) {
+    return 0;
+  }
+
+  let filteredUsers = users.filter(function (user) {
+    return user.gender === gender;
+  });
+
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  let ages = filteredUsers.map(function (user) {
+    return user.age;
+  });
+
+  let averageAge = ages.reduce(function (sum, current, index, array) {
+    return sum + current / array.length;
+  }, 0);
+
+  return averageAge;
 }
