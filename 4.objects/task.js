0d52f3503bbe2c85e6f32b3d93f9771426a92d3b
eshkq <eshkaflex@yandex.ru>
2025-11-16 12:36:52 +0400

    Задача 1 (objects)

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,43 @@
+"use strict";
+
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (!this.marks) {
+    return;
+  }
+
+  for (let i = 0; i < marksToAdd.length; i++) {
+    this.marks.push(marksToAdd[i]);
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+
+  return sum / this.marks.length;
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
+};
